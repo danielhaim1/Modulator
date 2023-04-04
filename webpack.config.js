fedf75bd752576d0c43111d776e44fd0504975f2
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2023-04-04 11:31:49 +0200

    umd>amd

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,66 @@
-const path = require('path');
-const isDevelopment = process.env.NODE_ENV === 'development';
-const nodeExternals = require('webpack-node-externals');
-const TerserPlugin = require('terser-webpack-plugin');
-module.exports = [
-  {
-    mode: 'production',
-    target: 'web',
-    entry: './src/index.js',
-    output: {
-      filename: 'event.modulator.browser.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-env'
-                  , {
-                    targets: {
-                      browsers: ['last 2 versions', 'safari >= 7'],
-                    },
-                    modules: 'umd',
-                  }
-                , ]
-              , ],
+const path = require("path"),
+  isDevelopment = "development" === process.env.NODE_ENV,
+  nodeExternals = require("webpack-node-externals"),
+  TerserPlugin = require("terser-webpack-plugin");
+  
+module.exports = [{
+  mode: "production",
+  target: "web",
+  entry: "./src/index.js",
+  output: {
+    filename: "event.modulator.browser.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    minimize: !0,
+    minimizer: [new TerserPlugin]
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [["@babel/preset-env", {
+            targets: {
+              browsers: ["last 2 versions", "safari >= 7"]
             },
-          },
+            modules: "amd"
+          }, ], ]
         }
-      , ],
-    },
+      }
+    }, ]
   }
-  , {
-    mode: 'production',
-    target: 'node',
-    entry: './src/index.js',
-    output: {
-      filename: 'event.modulator.js',
-      path: path.resolve(__dirname, 'dist'),
-      libraryTarget: 'commonjs2',
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-env'
-                  , {
-                    targets: {
-                      node: '10',
-                    },
-                    modules: 'commonjs',
-                  }
-                , ]
-              , ],
+}, {
+  mode: "production",
+  target: "node",
+  entry: "./src/index.js",
+  output: {
+    filename: "event.modulator.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "commonjs2"
+  },
+  optimization: {
+    minimize: !0,
+    minimizer: [new TerserPlugin]
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [["@babel/preset-env", {
+            targets: {
+              node: "10"
             },
-          },
+            modules: "commonjs"
+          }, ], ]
         }
-      , ],
-    },
-    externals: [nodeExternals()],
-  }
-, ];
+      }
+    }, ]
+  },
+  externals: [nodeExternals()]
+}, ];
